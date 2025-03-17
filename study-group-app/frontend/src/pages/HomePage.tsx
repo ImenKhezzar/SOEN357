35a424a4cf3857f6d75ed9794be96d2a97c9cf58
Imen <khezzarimen@gmail.com>
2025-03-16 22:59:05 -0400

    home page merge fix

--- study-group-app/frontend/src/pages/HomePage.tsx
+++ study-group-app/frontend/src/pages/HomePage.tsx
@@ -1,64 +1,14 @@
-import "./App.css";
-import { useState } from "react";
-import { MeetingProvider } from "@videosdk.live/react-sdk";
-import { authToken, createMeeting } from "../API";
-import MeetingView from "../components/MeetingView";
+import React from 'react';
+import Logout from '../components/Logout';
+import '../App.css'
 
-function JoinScreen({
-  getMeetingAndToken,
-}: {
-  getMeetingAndToken: (meeting?: string) => void;
-}) {
-  const [meetingIdInput, setMeetingIdInput] = useState("");
-
-  return (
-    <div>
-      <button onClick={() => getMeetingAndToken()}>Create Meeting</button>
-      <input
-        type="text"
-        placeholder="Enter Meeting ID"
-        value={meetingIdInput}
-        onChange={(e) => setMeetingIdInput(e.target.value)}
-      />
-      <button onClick={() => getMeetingAndToken(meetingIdInput)}>
-        Join Meeting
-      </button>
-    </div>
-  );
-}
-
-function HomePage() {
-  const [meetingId, setMeetingId] = useState<string | null>(null);
-
-  const getMeetingAndToken = async (id?: string) => {
-    const meetingId =
-      id == null ? await createMeeting({ token: authToken }) : id;
-    setMeetingId(meetingId);
-  };
-
-  const onMeetingLeave = () => {
-    setMeetingId(null);
-  };
-
-  return authToken && meetingId ? (
-    <MeetingProvider
-      config={{
-        meetingId,
-        micEnabled: true,
-        webcamEnabled: true,
-        name: "Imen",
-      }}
-      token={authToken}
-    >
-      <p>
-        <strong>Meeting ID:</strong> {meetingId}
-      </p>
-      <MeetingView />
-      <button onClick={onMeetingLeave}>Leave Meeting</button>
-    </MeetingProvider>
-  ) : (
-    <JoinScreen getMeetingAndToken={getMeetingAndToken} />
-  );
-}
+const HomePage = () => {
+   
+    return (
+        <div className="homepage-container">
+            <Logout/>
+        </div>
+    );
+};
 
 export default HomePage;
