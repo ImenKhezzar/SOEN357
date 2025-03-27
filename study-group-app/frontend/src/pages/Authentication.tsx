ffbe6062e9e4ab7fd94d577a3366ee45fe1da0f6
Imen <khezzarimen@gmail.com>
2025-03-27 00:49:28 -0400

    Added logo to auth page

--- study-group-app/frontend/src/pages/Authentication.tsx
+++ study-group-app/frontend/src/pages/Authentication.tsx
@@ -1,26 +1,51 @@
-import React, { useState } from 'react';
-import SignIn from '../components/SignIn';
-import SignUp from '../components/SignUp';
-import '../App.css';
-const Authentication: React.FC = () => {
-    const [isSignIn, setIsSignIn] = useState(true);
+import React, { useState } from "react";
+import SignIn from "../components/SignIn";
+import SignUp from "../components/SignUp";
+import "../App.css";
+import logo from '../assets/image.png';
 
-    return (
-        <div className="authentication-container">
-            <h1 className="authentication-header">Welcome to LUMORA</h1>
-            <div className="white-box" style={{ width: '70vw', paddingTop: '5%', paddingBottom: '10%', maxWidth: '1280px' }}>
-            <div className="toggle">
-                <button onClick={() => setIsSignIn(true)} disabled={isSignIn} className='toggle-button'>
-                    Sign In
-                </button>
-                <button onClick={() => setIsSignIn(false)} disabled={!isSignIn} className='toggle-button'>
-                    Sign Up
-                </button>
-            </div>
-            {isSignIn ? <SignIn /> : <SignUp />}
-            </div>
+const Authentication: React.FC = () => {
+  const [isSignIn, setIsSignIn] = useState(true);
+
+  return (
+    <div className="authentication-container">
+      <h1 className="authentication-header">
+        Welcome to{" "}
+        <img
+          src={logo}
+          alt="Lumora"
+          className="authentication-logo"
+        />{" "}
+      </h1>
+      <div
+        className="white-box"
+        style={{
+          width: "70vw",
+          paddingTop: "5%",
+          paddingBottom: "10%",
+          maxWidth: "1280px",
+        }}
+      >
+        <div className="toggle">
+          <button
+            onClick={() => setIsSignIn(true)}
+            disabled={isSignIn}
+            className="toggle-button"
+          >
+            Sign In
+          </button>
+          <button
+            onClick={() => setIsSignIn(false)}
+            disabled={!isSignIn}
+            className="toggle-button"
+          >
+            Sign Up
+          </button>
         </div>
-    );
+        {isSignIn ? <SignIn /> : <SignUp />}
+      </div>
+    </div>
+  );
 };
 
 export default Authentication;
