7779df6cd4e9b808c6fbfc9b1f9076cb33dc3005
Imen <khezzarimen@gmail.com>
2025-04-08 23:53:28 -0400

    delete

--- study-group-app/frontend/src/routes.tsx
+++ study-group-app/frontend/src/routes.tsx
@@ -1,28 +1,25 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import RequireAuth from './components/Authentication/RequireAuth';
-import Authentication from './pages/Authentication';
-import HomePage from './pages/HomePage';
-import MeetingRoom from './pages/MeetingRoom';
-import DashboardPage from './pages/Dashboard';
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import RequireAuth from "./components/Authentication/RequireAuth";
+import Authentication from "./pages/Authentication";
+import HomePage from "./pages/HomePage";
+import MeetingRoom from "./pages/MeetingRoom";
 
 const AppRoutes: React.FC = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Authentication />} />
-            <Route path="/auth" element={<Authentication />} />
+  return (
+    <Routes>
+      <Route path="/" element={<Authentication />} />
+      <Route path="/auth" element={<Authentication />} />
 
-            {/* Add path that requires authetication to be access */}
-            <Route element={<RequireAuth />}>
-            <Route path="/homepage" element={<HomePage />} />
-            <Route path="/room/:roomId" element={<MeetingRoom/>} />
-            <Route path="/dashboard" element={<DashboardPage />} />
-            </Route>
+      {/* Add path that requires authetication to be access */}
+      <Route element={<RequireAuth />}>
+        <Route path="/homepage" element={<HomePage />} />
+        <Route path="/room/:roomId" element={<MeetingRoom />} />
+      </Route>
 
-            <Route path="*" element={<h1>Not Found</h1>} />
-        </Routes>
-    );
+      <Route path="*" element={<h1>Not Found</h1>} />
+    </Routes>
+  );
 };
 
 export default AppRoutes;
