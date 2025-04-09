f7343cde93d5a01c35cfddbfef6007343dd3b25b
Imen <khezzarimen@gmail.com>
2025-04-08 23:52:48 -0400

    changed logout button color

--- study-group-app/frontend/src/components/Authentication/Logout.tsx
+++ study-group-app/frontend/src/components/Authentication/Logout.tsx
@@ -1,38 +1,46 @@
-import { useNavigate } from 'react-router-dom';
-import useAxiosPrivate from '../../hooks/useAxiosPrivate';
-import Button from '@mui/material/Button';
+import { useNavigate } from "react-router-dom";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import Button from "@mui/material/Button";
 import useAuth from "../../hooks/useAuth";
-import '../../App.css'; 
+import "../../App.css";
 
 const Logout = () => {
-    const navigate = useNavigate();
-    const axiosPrivate = useAxiosPrivate();
-    const authContext = useAuth();
-    if (!authContext) {
-        throw new Error('useAuth must be used within an AuthProvider');
+  const navigate = useNavigate();
+  const axiosPrivate = useAxiosPrivate();
+  const authContext = useAuth();
+  if (!authContext) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  const { setAuth } = authContext;
+
+  const handleLogout = async () => {
+    try {
+      const response = await axiosPrivate.get("/logout");
+      if (response.status === 204) {
+        setAuth(null);
+        navigate("/auth");
+      }
+    } catch (err) {
+      console.error("Failed to logout", err);
     }
-    const { setAuth } = authContext;
+  };
 
-
-    const handleLogout = async () => {
-        try {
-            const response = await axiosPrivate.get('/logout'); 
-            if(response.status === 204){
-                setAuth(null);
-                navigate('/auth');
-            }
-        } catch (err) {
-            console.error('Failed to logout', err);
-        }
-    };
-
-    return (
-        <div className="logout-button">
-            <Button variant="contained" onClick={handleLogout}>
-                Logout
-            </Button>
-        </div>
-    );
+  return (
+    <div className="logout-button">
+      <Button
+        variant="contained"
+        sx={{
+          textTransform: "none",
+          color: "#fff",
+          backgroundColor: "#9387B4",
+          border: "2px solid #9387B4",
+        }}
+        onClick={handleLogout}
+      >
+        Logout
+      </Button>
+    </div>
+  );
 };
 
 export default Logout;
